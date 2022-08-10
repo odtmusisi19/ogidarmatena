d397c23c443c78b13622ebad49f8d7b11d9d7eaa
unknown <ogidarmatena@gmail.com>
2022-08-10 22:10:29 +0800

    menambahkan async

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://kit.fontawesome.com/c8e4d183c2.js" async></script>
         {/* aktifkan jika ingin menggunakakn smooth scroll */}
-        <script src="https://min30327.github.io/luxy.js/dist/js/luxy.js"></script>
+        <script src="https://min30327.github.io/luxy.js/dist/js/luxy.js" async></script>
         <link rel="icon" href="/logo.ico" />
         <link rel="stylesheet" type="text/css" href="/public/css/demo.css"></link>
       </Head>
@@ -21,9 +21,9 @@ const Home: NextPage = () => {
       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js" async></script>
       <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12" async></script>
       <Script id="my-script" async>{`
-
       //aktifkan luxy.init() jika ingin menggunakan smooth Scrool akan tetapi scroll into view akan tidak berfungsi
        luxy.init();
+
       $(document).ready(function() {
         $(".logo").click(function() {
           $( "ul li" ).slideToggle( "slow");
